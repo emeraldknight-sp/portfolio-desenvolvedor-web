c0ff5011074fa47bbff13d42240aa0508528b2bd
David Almeida <contato.davidalmeida@outlook.com>
2022-05-16 22:32:25 -0300

    ✅ Add: homepage and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleClick = () => {
+    console.log("Oi")
+  }
+
+  const navigate = useNavigate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="large__container">
+      <header className="header">
+        <div className="container">
+          <figure className="header__figure">
+            <img className="header__image" src='' alt='logo' />
+          </figure>
+          <ul className="header__menu">
+            <li className="menu__itens" onClick={() => handleClick()}> Home </li>
+            <li className="menu__itens" onClick={() => handleClick()}> Projetos </li>
+            <li className="menu__itens" onClick={() => handleClick()}> Habilidades </li>
+            <li className="menu__itens" onClick={() => handleClick()}> Contato </li>
+          </ul>
+        </div>
       </header>
+      <main className="main">
+        <div className="container">
+          <section className="section__about">
+            <article className=""></article>
+            <article className=""></article>
+          </section>
+          <section className="section__hard__skills">
+            <article className=""></article>
+            <article className=""></article>
+          </section>
+          <section className="section__soft__skills">
+            <article className=""></article>
+            <article className=""></article>
+          </section>
+        </div>
+      </main>
+      <footer className="footer">
+        <div className="container">
+          <ul className="footer__menu">
+            <li className="menu__itens--footer" onClick={() => }>Linkedin</li>
+            <li className="menu__itens--footer">Github</li>
+            <li className="menu__itens--footer">Discord</li>
+            <li className="menu__itens--footer">Email</li>
+          </ul>
+        </div>
+      </footer>
     </div>
   );
 }
