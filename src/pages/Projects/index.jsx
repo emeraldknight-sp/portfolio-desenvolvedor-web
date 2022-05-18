2623e21846e30958faa2ace94d14adfa9e31ada5
David Almeida <contato.davidalmeida@outlook.com>
2022-05-17 21:03:16 -0300

    ✅ Add: projects components

---
+++ src/pages/Projects/index.jsx
@@ -0,0 +1,25 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+
+const Projects = () => {
+  return(
+    <>
+      {/* HEADER */}
+      <Header />
+
+      {/* MAIN */}
+      <Main>
+          
+
+          
+      </Main>
+
+      {/* FOOTER */}
+      <Footer />
+
+    </>
+  )
+}
+
+export default Projects;
