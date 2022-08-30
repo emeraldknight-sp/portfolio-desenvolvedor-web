8af0d2cd3f3c0edaae2712cfbf1f5a06c65f8848
David Almeida <contato.davidalmeida@outlook.com>
2022-08-30 17:52:00 -0300

    ✅ Add: projects page file

---
+++ src/pages/Projects/index.js
@@ -0,0 +1,17 @@
+import Footer from "../../components/Footer/";
+import Header from "../../components/Header/";
+import Main from "../../components/Main/";
+
+const Projects = () => {
+  return (
+    <div className="large__container">
+      <Header />
+      <Main>
+        <h1>Projects</h1>
+      </Main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Projects;
