44076fdfaa74cb840968c10f4004a127c07769ad
David Almeida <contato.davidalmeida@outlook.com>
2022-08-30 17:54:36 -0300

    ✅ Add: contact page file

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,18 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+
+
+const Contact = () => {
+  return (
+    <div className="large__container">
+      <Header />
+      <Main>
+        <h1>Contact</h1>
+      </Main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Contact;
