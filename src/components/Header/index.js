486fe817a92f36c0dbf9c012a62422f18c791655
David Almeida <contato.davidalmeida@outlook.com>
2022-09-01 17:19:13 -0300

    ✅ Uodate: adding one more shortcut in the header menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import { AiOutlineHome } from 'react-icons/ai';
 import { MdOutlineWorkOutline } from 'react-icons/md';
 import { GrInfo } from 'react-icons/gr';
 import { BsTelephone } from 'react-icons/bs';
+import { FiTool } from 'react-icons/fi'
 
 import Logo from '../../images/svg/branding.svg';
 import { useNavigate } from 'react-router-dom';
@@ -30,6 +31,10 @@ const Header = () => {
           <li className="menu__option--header" onClick={() => handleClick("/projects")}>
             <MdOutlineWorkOutline />
             <span>Projetos</span>
+          </li>
+          <li className="menu__option--header" onClick={() => handleClick("/tecnologies")}>
+            <FiTool />
+            <span>Ferramentas de Trabalho</span>
           </li>
           <li className="menu__option--header" onClick={() => handleClick("/about")}>
             <GrInfo />
