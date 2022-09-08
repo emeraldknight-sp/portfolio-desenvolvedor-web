5c8fd266ed6280ea7c6c63b75e68325a451a3576
David Almeida <contato.davidalmeida@outlook.com>
2022-09-08 17:46:03 -0300

    ✅ Add: back-to-top button

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,24 @@
-import '../../styles/style.css';
+import { MdOutlineKeyboardArrowUp } from "react-icons/md";
+import "../../styles/style.css";
 
 const Main = ({ children }) => {
+  
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <main className="main">
-        { children }
+      {children}
+      <button
+        className="scroll_to_top"
+        type="button"
+        onClick={() => scrollToTop()}
+      >
+        <MdOutlineKeyboardArrowUp />
+      </button>
     </main>
-  )
-}
+  );
+};
 
 export default Main;
