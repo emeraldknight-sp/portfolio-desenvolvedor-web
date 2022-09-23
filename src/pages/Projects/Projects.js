b61c82590c3756bf1ff4197140f521fb7571cf46
David Almeida <contato.davidalmeida@outlook.com>
2022-09-23 04:07:35 -0300

    ✅ Add: Projects component

---
+++ src/pages/Projects/Projects.js
@@ -0,0 +1,17 @@
+import { Header } from '../../components/Header'
+import { Main } from '../../components/Main'
+import { Footer } from '../../components/Footer'
+
+import './Projects.style.css'
+
+export const Projects = () => {
+  return (
+    <div className="large__container">
+      <Header />
+      <Main>
+        <h1>Projects</h1>
+      </Main>
+      <Footer />
+    </div>
+  )
+}
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,17 +1,1 @@
-import Footer from "../../components/Footer/";
-import Header from "../../components/Header/";
-import Main from "../../components/Main/";
-
-const Projects = () => {
-  return (
-    <div className="large__container">
-      <Header />
-      <Main>
-        <h1>Projects</h1>
-      </Main>
-      <Footer />
-    </div>
-  )
-}
-
-export default Projects;
+export * from './Projects'
