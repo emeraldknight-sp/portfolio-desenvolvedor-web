ad78fdcb31fcc5a8e2104294c62a10247fa5920d
David Almeida <contato.davidalmeida@outlook.com>
2022-09-23 05:12:19 -0300

    ✅ Update: structuring and organizing content

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,6 +1,10 @@
+import { Container } from '../../components/Container'
 import { Header } from '../../components/Header'
 import { Main } from '../../components/Main'
 import { Footer } from '../../components/Footer'
+
+import { completed, inProgress, stopped } from './Projects.mock'
+import { CardProject } from '../../components/CardProject'
 
 import './Projects.style.css'
 
@@ -9,7 +13,77 @@ export const Projects = () => {
     <div className="large__container">
       <Header />
       <Main>
-        <h1>Projects</h1>
+        <Container>
+          <div className="about__projects">
+            <h3 className="projects__title">Projetos</h3>
+            <div className="completed">
+              <h4 className="projects__sub-title">Concluídos</h4>
+              <div className="projects-completed">
+                {completed.length ? (
+                  completed.map((project) => (
+                    <CardProject
+                      key={project.id}
+                      title={project.title}
+                      image={project.image}
+                      techs={project.techs}
+                      status={project.status}
+                      urlGitHub={project.urlGitHub}
+                      urlVercel={project.urlVercel}
+                    />
+                  ))
+                ) : (
+                  <span className="error-not-found">
+                    Nenhum trabalho para mostrar
+                  </span>
+                )}
+              </div>
+            </div>
+            <div className="in-progress">
+              <h4 className="projects__sub-title">Em desenvolvimento</h4>
+              <div className="projects-in-progress">
+                {inProgress.length > 0 ? (
+                  inProgress.map((project) => (
+                    <CardProject
+                      key={project.id}
+                      title={project.title}
+                      image={project.image}
+                      techs={project.techs}
+                      status={project.status}
+                      urlGitHub={project.urlGitHub}
+                      urlVercel={project.urlVercel}
+                    />
+                  ))
+                ) : (
+                  <span className="error-not-found">
+                    Nenhum trabalho para mostrar
+                  </span>
+                )}
+              </div>
+            </div>
+            <div className="stopped">
+              <h4 className="projects__sub-title">Parados</h4>
+              <div className="projects-stopped">
+                {stopped.length ? (
+                  stopped.map((project) => (
+                    <CardProject
+                      key={project.id}
+                      title={project.title}
+                      image={project.image}
+                      techs={project.techs}
+                      status={project.status}
+                      urlGitHub={project.urlGitHub}
+                      urlVercel={project.urlVercel}
+                    />
+                  ))
+                ) : (
+                  <span className="error-not-found">
+                    Nenhum trabalho para mostrar
+                  </span>
+                )}
+              </div>
+            </div>
+          </div>
+        </Container>
       </Main>
       <Footer />
     </div>
---
+++ src/pages/Projects/Projects.mock.js
@@ -0,0 +1,104 @@
+import MundoInvertido from '../../images/projects/mundo-invertido.png'
+import EmeraldBooks from '../../images/projects/emerald-books.png'
+import StarWars from '../../images/projects/star-wars.png'
+import KenzieCars from '../../images/projects/kenzie-cars.png'
+import BurguerKenzie from '../../images/projects/kenzie-burguer.png'
+import NuKenzie from '../../images/projects/nu-kenzie.png'
+import KenzieHub from '../../images/projects/kenzie-hub.png'
+import ContaPalavras from '../../images/projects/conta-palavras.png'
+import clickJogos from '../../images/projects/click-jogos.png'
+
+export const completed = [
+  {
+    id: 1,
+    urlVercel: 'https://semana-frontend-mundo-invertido-nine.vercel.app/',
+    urlGitHub:
+      'https://github.com/emeraldknight-sp/semana-frontend-mundo-invertido',
+    title: 'Mundo Invertido | Switch Mode',
+    techs: 'HTML5, CSS3, JS',
+    status: 'Concluído',
+    image: MundoInvertido,
+  },
+  {
+    id: 3,
+    urlVercel: 'https://star-wars-website-delta.vercel.app/',
+    urlGitHub: 'https://github.com/emeraldknight-sp/star-wars-website',
+    title: 'Star Wars | Planets API',
+    techs: 'React, Styled-Components',
+    status: 'Concluído',
+    image: StarWars,
+  },
+  {
+    id: 6,
+    urlVercel: 'https://nu-kenzie-liart.vercel.app/',
+    urlGitHub:
+      'https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s1-nu-kenzie-emeraldknight-sp',
+    title: 'Nu Kenzie | FinTech',
+    techs: 'React, CSS3',
+    status: 'Concluído',
+    image: NuKenzie,
+  },
+  {
+    id: 7,
+    urlVercel: 'https://github-kenzie.vercel.app/',
+    urlGitHub:
+      'https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s2-kenzie-hub-emeraldknight-sp',
+    title: 'Kenzie Hub | User Login',
+    techs: 'React, Styled-Components',
+    status: 'Concluído',
+    image: KenzieHub,
+  },
+  {
+    id: 8,
+    urlVercel: 'https://emeraldknight-sp.github.io/ContaPalavras/',
+    urlGitHub: 'https://github.com/emeraldknight-sp/ContaPalavras',
+    title: 'Counter | Logic',
+    techs: 'HTML5, CSS3, JS',
+    status: 'Concluído',
+    image: ContaPalavras,
+  },
+  {
+    id: 9,
+    urlVercel: 'https://emeraldknight-sp.github.io/JogosDeAzar/',
+    urlGitHub: 'https://github.com/emeraldknight-sp/JogosDeAzar',
+    title: 'Click Jogos | DOM',
+    techs: 'HTML5, CSS3, JS, DOM',
+    status: 'Concluído',
+    image: clickJogos,
+  },
+]
+
+export const stopped = []
+
+export const inProgress = [
+  {
+    id: 2,
+    urlVercel:
+      'https://kenzieshop-context-api-49byuege0-emeraldknight-sp.vercel.app/',
+    urlGitHub:
+      'https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s3-kenzishop-com-context-api-emeraldknight-sp',
+    title: 'Emerald Books | e-Commerce',
+    techs: 'React, Styled-Components, Context API',
+    status: 'Interrompidos',
+    image: EmeraldBooks,
+  },
+  {
+    id: 4,
+    urlVercel: 'https://kenzieshop-kenzie-cars.vercel.app/',
+    urlGitHub:
+      'https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s3-kenzieshop-emeraldknight-sp',
+    title: 'Kenzie Cars | e-Commerce',
+    techs: 'React, Styled-Components, REDUX',
+    status: 'Interrompidos',
+    image: KenzieCars,
+  },
+  {
+    id: 5,
+    urlVercel: 'https://vercel.com/emeraldknight-sp/hamburgueria',
+    urlGitHub: 'https://github.com/emeraldknight-sp/ecommerce-kenzieburguer',
+    title: 'Burguer Kenzie | e-Commerce',
+    techs: 'React, CSS3',
+    status: 'Interrompidos',
+    image: BurguerKenzie,
+  },
+]
