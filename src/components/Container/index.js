1a1fdb89daa1d27ea2fb402c6de11634ca9ce85b
David Almeida <contato.davidalmeida@outlook.com>
2022-09-23 02:20:22 -0300

    🎨 Format of the code: Refactor

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,13 +1,1 @@
-
-const Container = ({ children, ...rest }) => {
-
-  const {className} = rest
-
-  return (
-    <article className={className}>
-      {children}
-    </article>
-  )
-}
-
-export default Container;
+export * from './Container'
