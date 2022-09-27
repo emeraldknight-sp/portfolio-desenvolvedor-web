fdb10c4e2df950dea02c17e243ac5bb2c4134e36
David Almeida <contato.davidalmeida@outlook.com>
2022-09-27 17:00:06 -0300

    ✅ Add: card academic component

---
+++ src/components/CardAcademic/CardAcademic.js
@@ -0,0 +1,18 @@
+import './CardAcademic.style.css'
+
+export const CardAcademic = ({ image, name, degree, duration, study }) => {
+  return (
+    <div className="card-academic">
+      <figure className="card-academic__figure">
+        <img className="card-academic__image" src={image} alt={name} />
+      </figure>
+      <div className="card-academic__details">
+        <h6 className="card-academic__title">{name}</h6>
+        <p>
+          {degree}, {study}
+        </p>
+        <p>Carga horária: {duration}</p>
+      </div>
+    </div>
+  )
+}
