b6d674ce8eba9b3c2563a33c2db06590c8a34658
David Almeida <contato.davidalmeida@outlook.com>
2023-01-07 04:21:57 -0300

    ♻️ Refactor code

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,18 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import LogoUrl from "../../images/png/emerald.png";
 
 import "./Logo.style.css";
 
 export const Logo = () => {
-  const navigate = useNavigate();
 
   return (
-    <a href="/" className="logo" onClick={() => navigate("/")}>
+    <Link to="/" className="logo">
       <figure>
         <img className="image" src={LogoUrl} alt="logo" />
       </figure>
       <h5 className="title">David Almeida</h5>
-    </a>
+    </Link>
   );
 };
