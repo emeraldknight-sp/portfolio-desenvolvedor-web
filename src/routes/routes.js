ad51ed332e084552ffcc6fb61a0e2ae5b5552fdc
David Almeida <contato.davidalmeida@outlook.com>
2023-01-07 03:35:53 -0300

    🎨 Improve structure/Format of the code

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,10 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-import { Homepage } from '../pages/Home'
-import { Projects } from '../pages/Projects'
-import { Tools } from '../pages/Tools'
-import { About } from '../pages/About'
-import { Contact } from '../pages/Contact'
+import { Homepage } from "../pages/Home";
+import { Projects } from "../pages/Projects";
+import { Tools } from "../pages/Tools";
+import { About } from "../pages/About";
+import { Contact } from "../pages/Contact";
 
 export const Root = () => {
   return (
@@ -15,5 +15,5 @@ export const Root = () => {
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
-  )
-}
+  );
+};
